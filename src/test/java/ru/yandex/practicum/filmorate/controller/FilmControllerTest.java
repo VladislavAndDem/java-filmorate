bd760530b233a00647586b68c739a231d0545f70
Vladislav.D <smachvlad@yandex.ru>
2025-02-19 20:56:30 +0300

    Checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
